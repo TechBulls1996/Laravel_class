79746943241db44804d2c25a0cc82a3982309b4e
Surya Pratap <akhilesh-at-652711644762>
2022-05-14 13:31:14 +0530

    uploading project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/main_template')
+
+@section('main_page')
+
+<section class="container">
+   
+    <h2>Hello</h2>
+
+
+
+</section>
+@endsection
---
+++ resources/views/layouts/main_template.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?= asset('/css/bootstrap.min.css') ?>">
+</head>
+<body>
+    
+<!-- header -->
+@include('includes/header')
+
+
+@yield('main_page')
+
+<!-- footer -->
+@include('includes/footer')
+
+</body>
+</html>
